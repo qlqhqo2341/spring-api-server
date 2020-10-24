8dcc76057ad0516041ba928cb8d78715517983ba
jeonghan-lee <qlqhqo2341@naver.com>
2020-10-25 02:02:13 +0900

    로그추가, pid쿼리 방법 변경

--- .config/service.sh
+++ .config/service.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 APPLICATION_JAR="${APPLICATION_JAR:-./target.jar}";
+APPLICATION_LOG="${APPLICATION_LOG:-./server.log}";
 APPLICATION_PID="${APPLICATION_PID:-./server.pid}";
 
 function stop(){
@@ -11,8 +12,9 @@ function stop(){
 
 function start(){
   echo '--start of start--'
-  nohup java -Dserver.port=8082 -jar $APPLICATION_JAR &
-  echo $! > $APPLICATION_PID
+  java -Dserver.port=8082 -jar $APPLICATION_JAR > $APPLICATION_LOG 2>&1 &
+  SERVER_PID=$!
+  echo $SERVER_PID > $APPLICATION_PID
   echo '--  end of start--'
 }
 
