67e2963b03a0823866d5facf7d0e4c198e5bb72b
jeonghan-lee <qlqhqo2341@naver.com>
2020-10-25 00:48:19 +0900

    서비스 스크립트 추가

---
+++ .config/service.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+APPLICATION_JAR="../target/api-0.0.1-SNAPSHOT.jar"
+
+function stop() {
+  echo '--start of stop---'
+  kill -9 `cat ./server.pid`
+  echo '--  end of stop---'
+}
+
+function start() {
+  echo '--start of start--'
+  nohup java -Dserver.port=8082 -jar $APPLICATION_JAR > ./server.log 2>&1 &
+  echo $! > server.pid
+  echo '--  end of start--'
+}
+
+if [[ $# -ne 1 ]];
+then
+  echo "usage : $0 command ";
+  echo "command : start stop restart";
+elif [ $1 == 'start' ]
+then
+  start;
+elif [ $1 == 'stop' ]
+then
+  stop;
+elif [ $1 == 'restart' ]
+then
+  stop;
+  start;
+else
+  echo "no command";
+fi;
