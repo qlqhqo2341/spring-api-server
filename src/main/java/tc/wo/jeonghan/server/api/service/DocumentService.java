b04abb3ede9e635a09e102c33070fcb329d340f5
jeonghan-lee <qlqhqo2341@naver.com>
2020-10-25 22:37:42 +0900

    현재 값에 시간값도 추가하도록 변경

--- src/main/java/tc/wo/jeonghan/server/api/service/DocumentService.java
+++ src/main/java/tc/wo/jeonghan/server/api/service/DocumentService.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import tc.wo.jeonghan.server.api.domain.Document;
 
+import java.time.LocalDateTime;
+
 @Service
 public class DocumentService {
 
     @GetMapping("/document")
     public Document getDocument(int id) {
-        Document document = new Document(id, "untitled", "blank text", null);
+        Document document = new Document(id, "untitled", "blank text", LocalDateTime.now());
         return document;
     }
 }
